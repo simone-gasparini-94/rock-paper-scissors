972402beafb2f29510fcd04637d62b0c7817d732
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2024-09-16 15:37:38 +0200

    added function for the computer choice with a randomizer (Math.random)

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getComputerChoice() {
-    return Math.random();
+    let randomNumber = Math.random();
+    if (randomNumber < 0.33333) {
+        return "rock";
+    } else if (randomNumber > 0.33333 && randomNumber < 0.66666) {
+        return "paper";
+    } else {
+        return "scissors"
+    }    
 }
 console.log(getComputerChoice());
