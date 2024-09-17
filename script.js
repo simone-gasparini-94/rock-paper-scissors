b9321b8564f864223720e9f057dd34eebbbaa4e8
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2024-09-17 13:58:51 +0200

    added determine winner function

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getHumanChoice() {
-    let playerChoice = prompt("rock, paper, or scissors?");
-    return playerChoice;
+    let humanChoice = prompt("rock, paper, or scissors?");
+    return humanChoice;
 }
-console.log("Your choice: " + getHumanChoice());
 
 function getComputerChoice() {
     let randomNumber = Math.random()*3;
@@ -14,5 +16,24 @@ function getComputerChoice() {
         return "scissors";
     }    
 }
-console.log("Computer choice: " + getComputerChoice());
 
+let player = getHumanChoice()
+let computer = getComputerChoice();
+
+console.log("Your choice: " + player);
+console.log("Computer choice: " + computer);
+
+function determineWinner(player, computer) {
+    if (player === computer) {
+        return "It's a tie!";
+    } else if (
+        (player === "rock" && computer === "scissors") ||
+        (player === "paper" && computer === "rock") ||
+        (player === "scissors" && computer === "paper")
+    ) {
+        return "Player wins!";
+    } else {
+        return "Computer wins!";
+    }
+}
+console.log(determineWinner(player, computer));
